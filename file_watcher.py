4de4e7886fb7da2fcb542a2035e9f00c1d63401c
Henrique-R-Lima <rick.rlima@hotmail.com>
2025-05-23 10:23:16 -0300

    Watcher initial test + launch.json for faster debbuging

--- file_watcher.py
+++ file_watcher.py
@@ -1,15 +1,37 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import time
-import requests
+from datetime import datetime
+import time, os
+
+# Program working directory
+WORKING_DIR = os.getcwd()
+
+# Directory to be watched
+WATCH_PATH = os.path.join(WORKING_DIR, "test_chamber")
 
 class SyncHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print(f"File changed: {event.src_path}")
-        # Sync logic here (upload to server or peer)
+    def on_any_event(self, event):
+
+        # Element that caused the event
+        element_str = event.src_path.removeprefix(os.path.join(WORKING_DIR,""))
+
+        # Event type
+        event_str = event.event_type
+
+        # Ignore changes to observed directory
+        if element_str == "test_chamber":
+            return
+        
+        element_str = element_str.removeprefix("test_chamber/")
+
+        # Current time
+        timestamp = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+
+        # Log changes
+        print(f"\'{element_str}\' {event_str} at {timestamp}")
 
 observer = Observer()
-observer.schedule(SyncHandler(), path="/path/to/watch", recursive=True)
+observer.schedule(SyncHandler(), path=WATCH_PATH, recursive=True)
 observer.start()
 
 try:
